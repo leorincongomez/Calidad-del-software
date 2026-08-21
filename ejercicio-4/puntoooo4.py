d7eaa06c56bc5813c8efdfe70b46cd65699510bd
LisbethMoore <lisbeth.moore.s@uniautonoma.edu.co>
2026-08-21 15:37:21 -0500

    eliminar carpetas compañeros

--- ejercicio-4/puntoooo4.py
+++
@@ -1,9 +0,0 @@
-class EstadisticaSimple:
-
-	def __init__(self, num1, num2, num3):
-		self.num1 = num1
-		self.num2 = num2
-		self.num3 = num3
-
-	def calcularPromedio(self):
-		return (self.num1 + self.num2 + self.num3) / 3
