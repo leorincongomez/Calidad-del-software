3dc33a4188b29dc76a51b3bff7ba1519549f30aa
JuanLuligo <juan.luligo@uniautonoma.edu.co>
2026-08-21 13:05:46 -0500

    ejercicio 8 de nuevo

---
+++ ejercicio8/ejercicio8.py
@@ -0,0 +1,17 @@
+entradaUsuario = input("si quiere sacar el area ingresa A y si quieres sacar la circunferencia ingresa C: ")
+
+def area(radio):
+    area = 3.1416*radio**2
+    print(f"el area de tu circulo es: {area}")
+
+def circunferencia(radio):
+    circunferencia = 2*3.1416*radio
+    print(f"la circunferencia de tu circulo es {circunferencia}")
+
+
+if entradaUsuario.upper() == "A":
+    radio = float(input("ingresa el radio de tu circulo para realizar la operacion: "))
+    area(radio)
+elif entradaUsuario.upper() == "C":
+    radio = float(input("ingresa el radio de tu circulo para realizar la operacion: "))
+    circunferencia(radio)
